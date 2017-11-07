a9ec89e2a08f553d60778b859d27534be817b2cb
KinIcy NET <nkinicy@gmail.com>
2017-11-07 09:34:45 -0500

    added new dependences, updated app setup

--- web/app.js
+++ web/app.js
@@ -7,13 +7,25 @@
 // include packages required.
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const router = require('./routes');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 // app setup,
 const app = express();
+app.use(morgan('dev'));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/public`));
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'averyveryverysecretsecret',
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // routes setup
 app.use('/api', router);
