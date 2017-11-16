dd9888ee75fc5ac9e73ecdc9ec3bb8c53f4a6123
Daniel <danielhernandez002@hotmail.com>
2017-11-16 10:23:27 -0500

    Fix de conexion a la db, le agregue a donde se conecta

--- web/controllers/user.js
+++ web/controllers/user.js
@@ -1,11 +1,11 @@
-'use strict'
+
 
 const User = require('../models/user');
 const service = require('../services');
 
 function signUp(req, res) {
   const user = new User({
-    password : req.body.password,
+    password: req.body.password,
     name: req.body.name,
     familyName: req.body.familyName,
     dateOfBirth: req.body.dateOfBirth,
@@ -32,7 +32,47 @@ function signIn(req, res) {
   });
 }
 
+function userExist(req, res) {
+  if (!req.body.user) {
+    return res.status(400).send();
+  }
+  User.findOne({ user: req.body.user }, (err, user) => {
+    if (err) return res.status(500).send({ message: err });
+    if (!user) return res.status(200).send({ userExist: false });
+    return res.status(200).send({ userExist: true });
+  });
+}
+
+function emailExist(req, res) {
+  if (!req.body.email) {
+    return res.status(400).send();
+  }
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) return res.status(500).send({ message: err });
+    if (!user) return res.status(200).send({ emailExist: false });
+    returnres.status(200).send({ emailExist: true });
+  });
+}
+
+function getUser(req, res) {
+  User.findOne({ user: req.params.user })
+    .then((user) => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(404).send({ message: 'user dont exist' });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: 'error to find user' });
+    });
+}
+
+
 module.exports = {
   signUp,
   signIn,
+  userExist,
+  emailExist,
+  getUser,
 };
